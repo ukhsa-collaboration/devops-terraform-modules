b82cc7b63d096f89c75c1efa1d8f2cf1570767de
Klyde Moradeyo <klydemoradeyo@capacitas.co.uk>
2023-10-02 17:14:33 +0100

    Fix resource-name-prefix in terraform-modules

--- terraform-modules/subnet/main.tf
+++ terraform-modules/subnet/main.tf
@@ -2,7 +2,7 @@
 #     Naming Config      #
 ##########################
 module "resource_name_prefix" {
-  source = "git@github.com:UKHSA-Internal/devops-terraform-modules.git//terraform-modules/tags?ref=resource-name-prefix/vALPHA_0.0.0"
+  source = "git@github.com:UKHSA-Internal/devops-terraform-modules.git//terraform-modules/resource-name-prefix?ref=resource-name-prefix/vALPHA_0.0.0"
 
   name = var.name
   tags = var.tags

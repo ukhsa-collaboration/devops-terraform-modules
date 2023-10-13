4665622844a138b1d2a9485bbf48510a48c1c7b3
Alistair Masawi <alistair.masawi@ukhsa.gov.uk>
2023-10-13 12:23:30 +0100

    example run

---
+++ examples/terraform-config/example.tf
@@ -0,0 +1,21 @@
+module "tags" {
+  source = "git@github.com:UKHSA-Internal/devops-terraform-modules.git//terraform-modules/tags?ref=tags/vALPHA_0.0.0"
+
+  project     = var.project
+  client      = var.client
+  owner       = var.owner
+  environment = var.environment
+  additional_tags = {
+    "Purpose" = "Proof of Concept"
+  }
+}
+
+module "subnets" {
+  source = "git@github.com:UKHSA-Internal/devops-terraform-modules.git//terraform-modules/subnet?ref=subnet/vALPHA_0.0.1"
+
+  name               = var.name
+  vpc_id             = var.vpc_id
+  subnet_cidr_blocks = [var.subnet_1_cidr_block, var.subnet_2_cidr_block, var.subnet_3_cidr_block]
+
+  tags = module.tags.tags
+}

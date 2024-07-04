140baf55f5a847c12d551ab4e2b5e8f7d8c5281e
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2024-07-04 15:26:00 +0100

    Refactor to use reusable modules

--- terraform-modules/aws/state-file/examples/full/main.tf
+++ terraform-modules/aws/state-file/examples/full/main.tf
@@ -18,4 +18,44 @@ resource "aws_kms_key" "example" {
   deletion_window_in_days = 7
 }
 
+resource "aws_kms_key_policy" "example" {
+  key_id = aws_kms_key.example.id
+  policy = jsonencode({
+    Id = "example"
+    Statement = [
+      {
+        Sid = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "${aws_iam_user.example.arn}"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid = "Allow Logs Service to use the key"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.${data.aws_region.current.name}.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      }
+    ]
+    Version = "2012-10-17"
+  })
+}
+
 data "aws_region" "current" {}
--- terraform-modules/aws/state-file/main.tf
+++ terraform-modules/aws/state-file/main.tf
@@ -1,66 +1,4 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "${data.aws_caller_identity.current.account_id}-${var.region_name}-state"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  count  = var.enable_s3_bucket_versioning ? 1 : 0
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    #checkov:skip=CKV2_AWS_67:Using a CMK is decided by the caller of the module and creating one is out-of-scope here.
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = length(var.state_bucket_kms_key_id) > 0 ? "aws:kms" : "AES256"
-      kms_master_key_id = length(var.state_bucket_kms_key_id) > 0 ? var.state_bucket_kms_key_id : null
-    }
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    id     = "abort-incomplete-multipart-upload"
-    status = "Enabled"
-
-    abort_incomplete_multipart_upload {
-      days_after_initiation = 7
-    }
-
-    expiration {
-      days                         = 0
-      expired_object_delete_marker = true
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = 365
-    }
-
-    noncurrent_version_transition {
-      noncurrent_days = 30
-      storage_class   = "STANDARD_IA"
-    }
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
+data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "terraform_state_bucket" {
   statement {
@@ -85,50 +23,88 @@ data "aws_iam_policy_document" "terraform_state_bucket" {
       "${aws_s3_bucket.terraform_state.arn}/*"
     ]
   }
+}
 
-  statement {
-    sid = "require-tls"
+module "terraform_state" {
+  source = "terraform-aws-modules/s3-bucket/aws"
 
-    effect = "Deny"
+  bucket = "${data.aws_caller_identity.current.account_id}-${var.region_name}-state"
+  acl    = "private"
 
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
+  attach_policy                            = true
+  attach_public_policy                     = true
+  policy                                   = data.aws_iam_policy_document.terraform_state_bucket.json
+  attach_deny_insecure_transport_policy    = true
+  attach_require_latest_tls_policy         = true
+  attach_deny_incorrect_encryption_headers = true
+  attach_deny_incorrect_kms_key_sse        = true
+  attach_deny_unencrypted_object_uploads   = true
+  
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerEnforced"
+
+  versioning = {
+    enabled = true
+  }
+
+  logging = {
+    target_bucket = module.terraform_state_log.s3_bucket_id
+    target_prefix = "log/"
+    target_object_key_format = {
+      partitioned_prefix = {
+        partition_date_source = "DeliveryTime"
+      }
     }
+  }
 
-    actions = ["s3:*"]
-
-    resources = [
-      "${aws_s3_bucket.terraform_state.arn}/*"
-    ]
-
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["false"]
+  server_side_encryption_configuration = {
+    rule = {
+      #checkov:skip=CKV2_AWS_67:Using a CMK is decided by the caller of the module and creating one is out-of-scope here.
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = length(var.state_bucket_kms_key_id) > 0 ? var.state_bucket_kms_key_id : null
+        sse_algorithm     = length(var.state_bucket_kms_key_id) > 0 ? "aws:kms" : "AES256"
+      }
     }
   }
 }
 
-resource "aws_s3_bucket_policy" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  policy = data.aws_iam_policy_document.terraform_state_bucket.json
-}
+module "terraform_state_log" {
+    source = "terraform-aws-modules/s3-bucket/aws"
 
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket                  = aws_s3_bucket.terraform_state.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+    bucket = "${data.aws_caller_identity.current.account_id}-${var.region_name}-state-logs"
+    acl    = "private"
 
-resource "aws_s3_bucket_logging" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+    attach_policy                            = true
+    attach_public_policy                     = true
+    attach_deny_insecure_transport_policy    = true
+    attach_require_latest_tls_policy         = true
+    attach_deny_incorrect_encryption_headers = true
+    attach_deny_incorrect_kms_key_sse        = true
+    attach_deny_unencrypted_object_uploads   = true
+    
+    control_object_ownership = true
+    object_ownership         = "BucketOwnerEnforced"
 
-  target_bucket = aws_s3_bucket.terraform_state_bucket_logs.id
-  target_prefix = "log/"
+    access_log_delivery_policy_source_accounts = [data.aws_caller_identity.current.account_id]
+    access_log_delivery_policy_source_buckets = module.terraform_state.s3_bucket_arn
+
+    allowed_kms_key_arn = length(var.state_bucket_kms_key_id) > 0 ? var.state_bucket_kms_key_id : null
+
+    versioning = {
+      enabled = true
+    }
+
+    server_side_encryption_configuration = {
+      rule = {
+        #checkov:skip=CKV2_AWS_67:Using a CMK is decided by the caller of the module and creating one is out-of-scope here.
+        apply_server_side_encryption_by_default = {
+          kms_master_key_id = length(var.state_bucket_kms_key_id) > 0 ? var.state_bucket_kms_key_id : null
+          sse_algorithm     = length(var.state_bucket_kms_key_id) > 0 ? "aws:kms" : "AES256"
+        }
+      }
+    }
 }
+  
 
 resource "aws_dynamodb_table" "terraform_locks" {
   # checkov:skip=CKV_AWS_28:The state lock table is ephemeral and doesn't need PITR.
@@ -144,118 +120,5 @@ resource "aws_dynamodb_table" "terraform_locks" {
 
   server_side_encryption {
     enabled = true
-  }
-}
-
-resource "aws_s3_bucket" "terraform_state_bucket_logs" {
-  bucket = "${data.aws_caller_identity.current.account_id}-${var.region_name}-state-logs"
-}
-
-# TODO: Find policy on how long logs should be kept for and change this.
-resource "aws_s3_bucket_lifecycle_configuration" "terraform_state_bucket_logs" {
-  bucket = aws_s3_bucket.terraform_state_bucket_logs.id
-
-  rule {
-    id     = "expire-logs"
-    status = "Enabled"
-
-    expiration {
-      days                         = 90
-      expired_object_delete_marker = true
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = 30
-    }
-  }
-
-  rule {
-    id     = "abort-incomplete-multipart-upload"
-    status = "Enabled"
-
-    abort_incomplete_multipart_upload {
-      days_after_initiation = 7
-    }
-
-    expiration {
-      days                         = 0
-      expired_object_delete_marker = true
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = 365
-    }
-
-    noncurrent_version_transition {
-      noncurrent_days = 30
-      storage_class   = "STANDARD_IA"
-    }
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "terraform_state_bucket_logs" {
-  bucket = aws_s3_bucket.terraform_state_bucket_logs.id
-
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
-
-resource "aws_s3_bucket_policy" "terraform_state_bucket_logs" {
-  bucket = aws_s3_bucket.terraform_state_bucket_logs.id
-  policy = data.aws_iam_policy_document.terraform_state_bucket_logs.json
-}
-
-data "aws_iam_policy_document" "terraform_state_bucket_logs" {
-  statement {
-    sid = "require-tls"
-
-    effect = "Deny"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = ["s3:*"]
-
-    resources = [
-      "${aws_s3_bucket.terraform_state_bucket_logs.arn}/*"
-    ]
-
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["false"]
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "terraform_state_bucket_logs" {
-  bucket                  = aws_s3_bucket.terraform_state_bucket_logs.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_bucket_logs" {
-  bucket = aws_s3_bucket.terraform_state_bucket_logs.id
-
-  rule {
-    #checkov:skip=CKV2_AWS_67:Using a CMK is decided by the caller of the module and creating one is out-of-scope here.
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = length(var.logs_bucket_kms_key_id) > 0 ? "aws:kms" : "AES256"
-      kms_master_key_id = length(var.logs_bucket_kms_key_id) > 0 ? var.logs_bucket_kms_key_id : null
-    }
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state_bucket_logs" {
-  count  = var.enable_s3_bucket_versioning ? 1 : 0
-  bucket = aws_s3_bucket.terraform_state_bucket_logs.id
-
-  versioning_configuration {
-    status = "Enabled"
   }
 }

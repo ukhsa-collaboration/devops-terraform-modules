b36c7f65cb78d60bcaac18a9cc3585090e368626
fishere1 <Ellis.fisher@ukhsa.gov.uk>
2024-07-22 12:16:00 +0100

    fmt

--- terraform-modules/azure/state-file/main.tf
+++ terraform-modules/azure/state-file/main.tf
@@ -1,20 +1,20 @@
 resource "azurerm_resource_group" "this" {
-  name     = "${var.resource_group_name}"
+  name     = var.resource_group_name
   location = var.location
 }
 
 module "storage_account" {
   source = "git::https://github.com/Azure/terraform-azurerm-avm-res-storage-storageaccount.git?ref=e017ac14fec632b1ca48592396f0078aa4773630" # Version 0.2.0 released 28/06/2024
 
-  name                = "${var.storage_account_name}"
+  name                = var.storage_account_name
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 
-  account_tier = "Standard"
-  shared_access_key_enabled = "true" # shared access keys should be enabled to true when Azure AD is resolve. https://github.com/Azure/terraform-azurerm-avm-res-storage-storageaccount#:~:text=IMPORTANT%20We%20recommend%20using%20Azure%20AD%20authentication%20over%20Shared%20Key%20for%20provisioning%20Storage%20Containers%2C%20Blobs%2C%20and%20other%20items.
-  default_to_oauth_authentication = "false"
+  account_tier                      = "Standard"
+  shared_access_key_enabled         = "true" # shared access keys should be enabled to true when Azure AD is resolve. https://github.com/Azure/terraform-azurerm-avm-res-storage-storageaccount#:~:text=IMPORTANT%20We%20recommend%20using%20Azure%20AD%20authentication%20over%20Shared%20Key%20for%20provisioning%20Storage%20Containers%2C%20Blobs%2C%20and%20other%20items.
+  default_to_oauth_authentication   = "false"
   infrastructure_encryption_enabled = "true"
-  public_network_access_enabled = "true"
+  public_network_access_enabled     = "true"
   network_rules = {
     default_action = "Allow"
   }
@@ -31,7 +31,7 @@ module "storage_account" {
     expiration_period = "00.02:00:00"
   }
 
- containers = {
+  containers = {
     application = {
       name                  = "application"
       container_access_type = "private"
@@ -44,11 +44,11 @@ module "storage_account" {
       name                  = "state"
       container_access_type = "private"
     }
-}
+  }
 }
 
 resource "null_resource" "upload" {
   provisioner "local-exec" {
     command = "az storage blob upload --account-name ${var.storage_account_name} --container-name state --name terraform.tfstate --file ./terraform.tfstate --account-key ${data.azurerm_storage_account.this.primary_access_key} --overwrite"
-    }
-    }
+  }
+}

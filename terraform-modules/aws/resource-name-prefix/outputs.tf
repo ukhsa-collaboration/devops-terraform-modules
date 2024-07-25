9781d31f913c56dd8d1f6689945bc1e5e751c1b6
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2024-07-25 09:07:26 +0100

    Changes following review

--- terraform-modules/aws/resource-name-prefix/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "resource_name" {
-  description = "Resource Name Prefix"
-  value       = local.name
-}
--- terraform-modules/azure/vnet-peering/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "peering_id" {
-  value       = azurerm_virtual_network_peering.vnet_peering.id
-  description = "The ID of the VNet peering."
-}

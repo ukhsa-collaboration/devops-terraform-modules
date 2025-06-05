0d84d8d5c304adae24bb04c22f9fb0fa8ae3e48b
seviourl <Lee.Seviour@ukhsa.gov.uk>
2025-06-05 14:05:44 +0100

    Update with mandatory tags

--- terraform-modules/helpers/tags/main.tf
+++ terraform-modules/helpers/tags/main.tf
@@ -1,15 +1,28 @@
+terraform {
+  required_version = ">= 1.0" 
+}
+
 locals {
   mandatory_tags = {
-    "Owner"           = var.owner
-    "BillingOwner"    = var.billing_owner
-    "Service"         = var.service
-    "Environment"     = var.environment
-    "Confidentiality" = var.confidentiality
+    "lz-TechOwner"                        = var.lz_tech_owner
+    "lz-CostCode"                         = var.lz_cost_code
+    "lz-BillingOwner"                     = var.lz_billing_owner
+    "lz-BackupPlan"                       = var.lz_backup_plan
+    "lz-GovernmentSecurityClassification" = var.lz_government_security_classification
+    "lz-Service"                          = var.lz_service
+    "lz-Environment"                      = var.lz_environment
+    "lz-SupportTier"                      = var.lz_support_tier
+    "lz-Team"                             = var.lz_team
+    "lz-Notification"                     = var.lz_notification
+    "lz-LeanIXId"                         = tostring(var.lz_lean_ix_id)
   }
 
   raw_optional_tags = {
-    "Directorate" = var.directorate
-    "Team"        = var.team
+    "lz-BusinessOwner"      = var.lz_business_owner
+    "lz-Schedule"           = var.lz_schedule
+    "lz-DataClassification" = var.lz_data_classification
+    "lz-GitCommitURL"       = var.lz_git_commit_url
+    "lz-HealthData"         = tostring(var.lz_health_data)
   }
   optional_tags = {
     for k, v in local.raw_optional_tags :
--- terraform-modules/helpers/tags/variables.tf
+++ terraform-modules/helpers/tags/variables.tf
@@ -1,74 +1,167 @@
-# Mandatory
-variable "owner" {
-  description = "(Mandatory) The owner of the workload or application"
+# LZ Mandatory Tags
+variable "lz_tech_owner" {
+  description = "(Mandatory) Technical person (or team) responsible for the workload. First point of contact for incidents and technical enquiries."
   type        = string
 
   validation {
-    condition     = length(var.owner) > 0 && can(regex("^[-a-zA-Z0-9_ @.]+$", var.owner))
-    error_message = "The owner variable must be set to a non-empty string and can only contain: a-z, A-Z, 0-9, _, -, @, ."
+    condition     = length(var.lz_tech_owner) > 0 && can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.lz_tech_owner))
+    error_message = "The lz_tech_owner variable must be a valid Civil Servant email address."
   }
 }
 
-variable "billing_owner" {
-  description = "(Mandatory) The owner of billing for the workload or application"
+variable "lz_cost_code" {
+  description = "(Mandatory) Finance for the given project/workload, validated during the keyholder process. Used to determine cost of cloud-based resources for a given workload or project."
   type        = string
 
   validation {
-    condition     = length(var.billing_owner) > 0 && can(regex("^[-a-zA-Z0-9_ @.]+$", var.billing_owner))
-    error_message = "The billing_owner variable must be set to a non-empty string and can only contain: a-z, A-Z, 0-9, _, -, @, ."
+    condition     = length(var.lz_cost_code) > 0 && can(regex("^[A-Z][0-9]{3}$", var.lz_cost_code))
+    error_message = "The lz_cost_code variable must be 1 letter followed by 3 digits (e.g., A123)."
   }
 }
 
-variable "service" {
-  description = "(Mandatory) Project name to which the resource is associated"
+variable "lz_billing_owner" {
+  description = "(Mandatory) To establish billing ownership across accounts/subscriptions to enable Showback/Chargeback strategy."
   type        = string
 
   validation {
-    condition     = length(var.service) > 0 && can(regex("^[-a-zA-Z0-9_ @.]+$", var.service))
-    error_message = "The service variable must be set to a non-empty string and can only contain: a-z, A-Z, 0-9, _, -, @, ."
+    condition     = length(var.lz_billing_owner) > 0 && can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.lz_billing_owner))
+    error_message = "The lz_billing_owner variable must be a valid Civil Servant email address."
   }
 }
 
-variable "environment" {
-  description = "(Mandatory) The environment for this deployment (Development, Test, Pre-Production or Production)"
+variable "lz_backup_plan" {
+  description = "(Mandatory) Describe the backup plan that should be associated with the resource."
   type        = string
 
   validation {
-    condition     = contains(["Development", "Test", "Pre-Production", "Production"], var.environment)
-    error_message = "The environment variable can only be set to 'Development', 'Test', 'Pre-Production' or 'Production'."
+    condition     = length(var.lz_backup_plan) > 0 && can(regex("^[-a-zA-Z0-9_ @.]+$", var.lz_backup_plan))
+    error_message = "The lz_backup_plan variable must be set to a non-empty string and can only contain: a-z, A-Z, 0-9, _, -, @, ."
   }
 }
 
-variable "confidentiality" {
-  description = "(Mandatory) The confidentiality level of the data being processed (OFFICIAL or OFFICIAL-SENSITIVE)"
+variable "lz_government_security_classification" {
+  description = "(Mandatory) Classification according to the Government Security Classification Policy."
   type        = string
 
   validation {
-    condition     = contains(["OFFICIAL", "OFFICIAL-SENSITIVE"], var.confidentiality)
-    error_message = "The confidentiality variable can only be set to 'OFFICIAL' or 'OFFICIAL-SENSITIVE'"
+    condition     = contains(["OFFICIAL", "OFFICIAL-SENSITIVE", "SECRET", "TOP SECRET"], var.lz_government_security_classification)
+    error_message = "The lz_government_security_classification variable must be one of: OFFICIAL, OFFICIAL-SENSITIVE, SECRET, TOP SECRET."
   }
 }
 
-# Optional
-variable "directorate" {
-  description = "(Optional) The directorate responsible for the workload or application"
+variable "lz_service" {
+  description = "(Mandatory) Name of the service that is delivered via this account/subscription."
+  type        = string
+
+  validation {
+    condition     = length(var.lz_service) > 0 && can(regex("^[-a-zA-Z0-9_ @.]+$", var.lz_service))
+    error_message = "The lz_service variable must be set to a non-empty string and can only contain: a-z, A-Z, 0-9, _, -, @, ."
+  }
+}
+
+variable "lz_environment" {
+  description = "(Mandatory) The environment that the account services, e.g, Pre-Production, Production, Development."
+  type        = string
+
+  validation {
+    condition     = contains(["Production", "PreProduction", "Development", "Sandbox"], var.lz_environment)
+    error_message = "The lz_environment variable must be one of: Production, PreProduction, Development, Sandbox."
+  }
+}
+
+variable "lz_support_tier" {
+  description = "(Mandatory) Support wrapper - RTO and RPO."
+  type        = string
+
+  validation {
+    condition     = contains(["Platinum", "Gold", "Silver", "Bronze"], var.lz_support_tier)
+    error_message = "The lz_support_tier variable must be one of: Platinum, Gold, Silver, Bronze."
+  }
+}
+
+variable "lz_team" {
+  description = "(Mandatory) To relate a workload back to a team in the absence of an existing person as an owner."
+  type        = string
+
+  validation {
+    condition     = length(var.lz_team) > 0 && can(regex("^[-a-zA-Z0-9_ @.]+$", var.lz_team))
+    error_message = "The lz_team variable must be set to a non-empty string and can only contain: a-z, A-Z, 0-9, _, -, @, ."
+  }
+}
+
+variable "lz_notification" {
+  description = "(Mandatory) Email address to send notifications to."
+  type        = string
+
+  validation {
+    condition     = length(var.lz_notification) > 0 && can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.lz_notification))
+    error_message = "The lz_notification variable must be a valid email address."
+  }
+}
+
+variable "lz_lean_ix_id" {
+  description = "(Mandatory) Reference to record in LeanIX."
+  type        = number
+
+  validation {
+    condition     = var.lz_lean_ix_id > 0
+    error_message = "The lz_lean_ix_id variable must be a valid positive number."
+  }
+}
+
+# LZ Optional Tags
+variable "lz_business_owner" {
+  description = "(Optional) Business person responsible for the workload. First point of contact for business related enquiries. Only required if different from TechOwner."
   type        = string
   default     = ""
 
   validation {
-    condition     = length(var.directorate) == 0 || can(regex("^[-a-zA-Z0-9_ @.]+$", var.directorate))
-    error_message = "The directorate variable can only contain: a-z, A-Z, 0-9, _, -, @, ."
+    condition     = length(var.lz_business_owner) == 0 || can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.lz_business_owner))
+    error_message = "The lz_business_owner variable must be a valid Civil Servant email address."
   }
 }
 
-variable "team" {
-  description = "(Optional) The team responsible for the workload or application"
+variable "lz_schedule" {
+  description = "(Optional) Define compute downtime schedule for cost savings on batch and non-prod workloads."
   type        = string
   default     = ""
 
   validation {
-    condition     = length(var.team) == 0 || can(regex("^[-a-zA-Z0-9_ @.]+$", var.team))
-    error_message = "The team variable can only contain: a-z, A-Z, 0-9, _, -, @, ."
+    condition     = length(var.lz_schedule) == 0 || can(regex("^[-a-zA-Z0-9_ @.]+$", var.lz_schedule))
+    error_message = "The lz_schedule variable can only contain: a-z, A-Z, 0-9, _, -, @, ."
+  }
+}
+
+variable "lz_data_classification" {
+  description = "(Optional) The classification or type of data that is being held."
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = length(var.lz_data_classification) == 0 || contains(["Personal", "Commercial", "Corporate"], var.lz_data_classification)
+    error_message = "The lz_data_classification variable must be one of: Personal, Commercial, Corporate."
+  }
+}
+
+variable "lz_git_commit_url" {
+  description = "(Optional) Resource level information to assist in tracking down issue root caused during incidents."
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = length(var.lz_git_commit_url) == 0 || can(regex("^https?://", var.lz_git_commit_url))
+    error_message = "The lz_git_commit_url variable must be a valid URL starting with http:// or https://."
+  }
+}
+
+variable "lz_health_data" {
+  description = "(Optional) Whether the data held is related to health information."
+  type        = bool
+  default     = null
+
+  validation {
+    condition     = var.lz_health_data == null || can(var.lz_health_data)
+    error_message = "The lz_health_data variable must be true or false."
   }
 }
 
